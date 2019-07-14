edf697b60a3b8bdd065b9e44f188a2663bc2e071
qwercik <erykandrzejewski@gmail.com>
2019-07-14 14:54:26 +0200

    Center text on meme

--- actions/generate.js
+++ actions/generate.js
@@ -1,4 +1,5 @@
 const memes = require('./../memes.json');
+const { createCanvas, loadImage } = require('canvas');
 
 module.exports = {
 	prefix: '!',
@@ -7,13 +8,17 @@ module.exports = {
 	description: 'Generate your own meme',
 	arguments: [
 		{name: 'memeName', pattern: /^(?!\s*$).+/},
-		{name: 'topText', pattern: /^(?!\s*$).+/},
-		{name: 'bottomText', pattern: /^(?!\s*$).+/},
+		{name: 'topText', pattern: /^.*$/},
+		{name: 'bottomText', pattern: /^.*$/},
 	],
 	callback: async function(parsed) {
 		const channel = parsed.message.channel;
-		const [memeName, topText, bottomText] = parsed.arguments;
 		
+		const memeName = parsed.arguments[0];
+		const topText = parsed.arguments[1] ? parsed.arguments[1] : '';
+		const bottomText = parsed.arguments[2] ? parsed.arguments[2] : '';
+
+
 		const meme = memes.memes.find(el => el.name === memeName);
 		if (!meme) {
 			channel.send('Such meme doesn\'t exist! Check memes\'s list.');
@@ -29,11 +34,16 @@ module.exports = {
 		ctx.strokeStyle = 'black';
 		ctx.lineWidth = 2;
 
-		ctx.font = 'bold 100% Impact';
-		ctx.fillText(topText.toUpperCase(), 0.05 * image.width, 100);
-		ctx.strokeText(topText.toUpperCase(), 0.05 * image.width, 100);
-		ctx.fillText(bottomText.toUpperCase(), 0.05 * image.width, image.height - 100);
-		ctx.strokeText(bottomText.toUpperCase(), 0.05 * image.width, image.height - 100);
+		const fontSize = image.height * 0.15;
+		ctx.font = `bold ${fontSize}px Impact`;
+
+		const topTextMargin = ctx.measureText(topText.toUpperCase()).width > 0.9 * image.width ? 0.05 * image.width : (image.width - ctx.measureText(topText.toUpperCase()).width) / 2;
+		ctx.fillText(topText.toUpperCase(), topTextMargin, image.height * 0.20, 0.9 * image.width);
+		ctx.strokeText(topText.toUpperCase(), topTextMargin, image.height * 0.20, 0.9 * image.width);
+
+		const bottomTextMargin = ctx.measureText(bottomText.toUpperCase()).width > 0.9 * image.width ? 0.05 * image.width : (image.width - ctx.measureText(bottomText.toUpperCase()).width) / 2;
+		ctx.fillText(bottomText.toUpperCase(), bottomTextMargin, image.height * 0.95, 0.9 * image.width);
+		ctx.strokeText(bottomText.toUpperCase(), bottomTextMargin, image.height * 0.95, 0.9 * image.width);
 
 		channel.send({
 			files: [{
