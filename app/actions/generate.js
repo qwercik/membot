2082c4b4f22f64a04b29dd4f5aca1114ef0e8f4e
qwercik <erykandrzejewski@gmail.com>
2019-07-29 01:06:00 +0200

    Move meme generating code to separate function

--- app/actions/generate.js
+++ app/actions/generate.js
@@ -1,4 +1,4 @@
-const { createCanvas, loadImage } = require('canvas');
+const MemeGenerator = require('app/utils/MemeGenerator');
 const memesStorage = require('app/utils/MemesStorage');
 const language = require('app/language');
 
@@ -26,33 +26,14 @@ module.exports = {
 			return;
 		}
 		
-		const image = await loadImage(`assets/${meme.path}`).catch(() => {
-			channel.send(language['meme_file_loading_error']);
-			return;
-		});
-
-		const canvas = createCanvas(image.width, image.height);
-		const ctx = canvas.getContext('2d');
-
-		ctx.drawImage(image, 0, 0);
-		ctx.fillStyle = 'white';
-		ctx.strokeStyle = 'black';
-		ctx.lineWidth = 2;
-
-		const fontSize = image.height * 0.15;
-		ctx.font = `bold ${fontSize}px Impact`;
-
-		const topTextMargin = ctx.measureText(topText.toUpperCase()).width > 0.9 * image.width ? 0.05 * image.width : (image.width - ctx.measureText(topText.toUpperCase()).width) / 2;
-		ctx.fillText(topText.toUpperCase(), topTextMargin, image.height * 0.20, 0.9 * image.width);
-		ctx.strokeText(topText.toUpperCase(), topTextMargin, image.height * 0.20, 0.9 * image.width);
-
-		const bottomTextMargin = ctx.measureText(bottomText.toUpperCase()).width > 0.9 * image.width ? 0.05 * image.width : (image.width - ctx.measureText(bottomText.toUpperCase()).width) / 2;
-		ctx.fillText(bottomText.toUpperCase(), bottomTextMargin, image.height * 0.95, 0.9 * image.width);
-		ctx.strokeText(bottomText.toUpperCase(), bottomTextMargin, image.height * 0.95, 0.9 * image.width);
+		const generatedMeme = await MemeGenerator.generate(meme.path, topText, bottomText)
+			.catch(error => {
+				channel.send(error);
+			});
 
 		channel.send({
 			files: [{
-				attachment: canvas.createJPEGStream(),
+				attachment: generatedMeme,
 			}],
 		}).catch(err => {
 			channel.send(language['meme_file_loading_error']);
---
+++ app/utils/MemeGenerator.js
@@ -0,0 +1,33 @@
+const { createCanvas, loadImage } = require('canvas');
+const language = require('app/language');
+const config = require('config/config.json');
+
+exports.generate = function(path, topText, bottomText) {
+	return new Promise(async (resolve, reject) => {
+		const image = await loadImage(`${config.memesFilesPath}/${path}`)
+			.catch(() => {
+				reject(language['meme_file_loading_error']);
+			});
+		
+		const canvas = createCanvas(image.width, image.height);
+		const ctx = canvas.getContext('2d');
+
+		ctx.drawImage(image, 0, 0);
+		ctx.fillStyle = 'white';
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 2;
+
+		const fontSize = image.height * 0.15;
+		ctx.font = `bold ${fontSize}px Impact`;
+
+		const topTextMargin = ctx.measureText(topText.toUpperCase()).width > 0.9 * image.width ? 0.05 * image.width : (image.width - ctx.measureText(topText.toUpperCase()).width) / 2;
+		ctx.fillText(topText.toUpperCase(), topTextMargin, image.height * 0.20, 0.9 * image.width);
+		ctx.strokeText(topText.toUpperCase(), topTextMargin, image.height * 0.20, 0.9 * image.width);
+
+		const bottomTextMargin = ctx.measureText(bottomText.toUpperCase()).width > 0.9 * image.width ? 0.05 * image.width : (image.width - ctx.measureText(bottomText.toUpperCase()).width) / 2;
+		ctx.fillText(bottomText.toUpperCase(), bottomTextMargin, image.height * 0.95, 0.9 * image.width);
+		ctx.strokeText(bottomText.toUpperCase(), bottomTextMargin, image.height * 0.95, 0.9 * image.width);
+		
+		resolve(canvas.createJPEGStream());
+	});
+};
