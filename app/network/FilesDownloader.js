ea840edb6c6a168c7ce5af1143d9a86153043cce
qwercik <erykandrzejewski@gmail.com>
2019-09-06 00:30:08 +0200

    Replace request module with axios and introduce async into FilesDownloader module

--- app/network/FilesDownloader.js
+++ app/network/FilesDownloader.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
-import request from 'request'
+import axios from 'axios'
 import language from 'app/language'
+import util from 'util'
 
 function getDownloadedFileType (response) {
   const contentType = response.headers['content-type']
@@ -8,7 +9,7 @@ function getDownloadedFileType (response) {
   if (contentType) {
     return contentType.split('/')[1]
   } else {
-    return response.requestUrl.split('.').pop().toLowerCase()
+    return response.config.url.split('.').pop().toLowerCase()
   }
 }
 
@@ -20,41 +21,46 @@ function forceEndingWith (string, forced) {
   return string
 }
 
-function download (url, saveDirectory, nameWithoutExtension) {
-  return new Promise((resolve, reject) => {
-    request.get(url)
-      .on('error', () => {
-        reject(new Error(language('request_error')))
-      })
-      .on('response', response => {
-        response.requestUrl = url
-        if (response.statusCode !== 200) {
-          reject(new Error(language('resource_not_exist_error')))
-        }
+async function download (url, saveDirectory, nameWithoutExtension) {
+  let response
+  try {
+    response = await axios({
+      url,
+      method: 'get',
+      responseType: 'stream'
+    })
+  } catch (error) {
+    throw new Error(language('request_error'))
+  }
 
-        const supportedFileTypes = ['jpg', 'jpeg', 'png', 'gif']
-        const fileType = getDownloadedFileType(response)
-        if (!supportedFileTypes.includes(fileType)) {
-          reject(new Error(language('unsupported_filetype_error')))
-        }
+  if (response.status !== 200) {
+    throw new Error(language('resource_not_exist_error'))
+  }
 
-        const fileName = nameWithoutExtension + '.' + fileType
-        const filePath = forceEndingWith(saveDirectory, '/') + fileName
+  const supportedFileTypes = ['jpg', 'jpeg', 'png', 'gif']
+  const fileType = getDownloadedFileType(response)
+  if (!supportedFileTypes.includes(fileType)) {
+    throw new Error(language('unsupported_filetype_error'))
+  }
 
-        try {
-          const stream = response.pipe(fs.createWriteStream(filePath))
-          stream.on('finish', () => {
-            resolve({
-              name: fileName,
-              path: filePath,
-              type: fileType
-            })
-          })
-        } catch (error) {
-          reject(new Error(language('no_permissions_to_save_downloaded_file')))
-        }
-      })
-  })
+  const fileName = nameWithoutExtension + '.' + fileType
+  const filePath = forceEndingWith(saveDirectory, '/') + fileName
+
+  let stream
+  try {
+    stream = response.data.pipe(fs.createWriteStream(filePath))
+  }
+  catch (error) {
+    throw new Error(language('no_permissions_to_save_downloaded_file'))
+  }
+
+  await (() => new Promise(resolve => stream.on('finish', resolve)))()
+
+  return {
+    name: fileName,
+    path: filePath,
+    type: fileType
+  }
 }
 
 export default { download }
